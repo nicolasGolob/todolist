f4377ff6449063f24a1a07944c86bc87834f81f1
Nicolas Golob <nicolasgolob@gmail.com>
2021-04-17 16:44:20 +0200

    enable the sending of to-do items

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,20 +6,38 @@ function Form(){
     const [dataArr, setDataArr] = useState([
         {text : 'Coding with react', id: uuidv4()},
         {text: 'Do the sports session', id: uuidv4()},
-        {text: 'yolo', id: uuidv4()}
+        {text: 'Send package for father', id: uuidv4()}
     ]);
+
+    const [stateInput, setStateInput] = useState();
 
     const handleDelete = (id)=>{
         const fileredState =  dataArr.filter(item=> item.id !== id)
         setDataArr(fileredState)
     }
+
+    const addTodo = (event) =>{
+        event.preventDefault();
+        const newArr = [...dataArr];
+        const newTodo = {};
+
+        newTodo.text=stateInput;
+        newTodo.id = uuidv4();
+        newArr.push(newTodo);
+        setDataArr(newArr);
+        setStateInput('')
+    }
+
+    const linkedInput = (event)=>{
+        setStateInput(event);
+    }
     
     return(
         <Fragment>
             <div className="m-auto col-12 col-sm-10 col-lg-10 px-4">
-            <form className="mb-3">
+            <form onSubmit={event=>addTodo(event)} className="mb-3">
                 <label htmlFor="todo" className="form-label mt-3"><h4>What's the plan for today ?</h4></label>
-                <input type="text" className="form-control" id="todo"/>
+                <input value={stateInput} onChange={event=>linkedInput(event.target.value)} type="text" className="form-control" id="todo"/>
                 <button className="btn btn-primary mt-3">Send</button>
             </form>
             <h4>To - do list : </h4>
